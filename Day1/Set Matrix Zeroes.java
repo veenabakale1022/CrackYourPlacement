3c5f14904803dfb07442e8fcaffe29ad4f1088b2
veenabakale1022 <91620607+veenabakale1022@users.noreply.github.com>
2023-09-07 23:06:35 +0530

    Create Set Matrix Zeroes.java

---
+++ Day1/Set Matrix Zeroes.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int col0 = 1; // Initialize col0 to 1
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    // Mark the i-th row
+                    matrix[i][0] = 0;
+                    // Mark the j-th column, except for the first column
+                    if (j != 0) {
+                        matrix[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+
+        // Set zeroes based on the marks in the first row and first column
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Set zeroes for the first row and the first column if needed
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (col0 == 0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
