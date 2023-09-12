81c43e30b2ccfd84582b8ba7bbe24f4b72271e44
veenabakale1022 <91620607+veenabakale1022@users.noreply.github.com>
2023-09-12 22:28:47 +0530

    Create Container With Most Water.java

---
+++ Day6/Container With Most Water.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            int minHeight = Math.min(height[left], height[right]);
+            int width = right - left;
+            int currentArea = minHeight * width;
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int maxArea = solution.maxArea(height);
+        System.out.println("The maximum area of the container is: " + maxArea);
+    }
+}
