da4c45c4f44dee578ad1daf631f13729f477e056
veenabakale1022 <91620607+veenabakale1022@users.noreply.github.com>
2023-09-13 23:21:42 +0530

    Update 4Sum.java

--- Day7/4Sum.java
+++ Day7/4Sum.java
@@ -1,63 +1,47 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+class Solution {
+  public List<List<Integer>> fourSum(int[] nums, int target) {
+    List<List<Integer>> ans = new ArrayList<>();
+    Arrays.sort(nums);
+    nSum(nums, 4, target, 0, nums.length - 1, new ArrayList<>(), ans);
+    return ans;
+  }
 
-public class FourSum {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-
-        if (nums == null || nums.length < 4)
-            return result;
-
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 3; i++) {
-            // Skip duplicates
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-
-            for (int j = i + 1; j < nums.length - 2; j++) {
-                // Skip duplicates
-                if (j > i + 1 && nums[j] == nums[j - 1])
-                    continue;
-
-                int left = j + 1;
-                int right = nums.length - 1;
-
-                while (left < right) {
-                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
-
-                    if (sum == target) {
-                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-
-                        // Skip duplicates
-                        while (left < right && nums[left] == nums[left + 1])
-                            left++;
-                        while (left < right && nums[right] == nums[right - 1])
-                            right--;
-
-                        left++;
-                        right--;
-                    } else if (sum < target) {
-                        left++;
-                    } else {
-                        right--;
-                    }
-                }
-            }
+  // Finds n numbers add up to the target in [l, r].
+  private void nSum(int[] nums, long n, long target, int l, int r, List<Integer> path,
+                    List<List<Integer>> ans) {
+    if (r - l + 1 < n || target < nums[l] * n || target > nums[r] * n)
+      return;
+    if (n == 2) {
+      // Similar to the sub procedure in 15. 3Sum
+      while (l < r) {
+        final int sum = nums[l] + nums[r];
+        if (sum == target) {
+          path.add(nums[l]);
+          path.add(nums[r]);
+          ans.add(new ArrayList<>(path));
+          path.remove(path.size() - 1);
+          path.remove(path.size() - 1);
+          ++l;
+          --r;
+          while (l < r && nums[l] == nums[l - 1])
+            ++l;
+          while (l < r && nums[r] == nums[r + 1])
+            --r;
+        } else if (sum < target) {
+          ++l;
+        } else {
+          --r;
         }
-
-        return result;
+      }
+      return;
     }
 
-    public static void main(String[] args) {
-        FourSum solution = new FourSum();
-        int[] nums = {1, 0, -1, 0, -2, 2};
-        int target = 0;
-        List<List<Integer>> result = solution.fourSum(nums, target);
-
-        for (List<Integer> quad : result) {
-            System.out.println(quad);
-        }
+    for (int i = l; i <= r; ++i) {
+      if (i > l && nums[i] == nums[i - 1])
+        continue;
+      path.add(nums[i]);
+      nSum(nums, n - 1, target - nums[i], i + 1, r, path, ans);
+      path.remove(path.size() - 1);
     }
+  }
 }
